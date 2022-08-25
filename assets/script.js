b6602fdb46acce38423815488026c0d9730b9d85
saul-paulus <saulpaulus17@icloud.com>
2022-08-26 03:53:31 +0900

    membuat skill

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,26 @@ function lickAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', lickAction));
+
+
+// function to skill
+
+const skillCard = document.getElementsByClassName('skill__card');
+const skillCardData = document.querySelectorAll('.skill__card-data');
+
+
+function toggleSkill() {
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillCard.length; i++) {
+        skillCard[i].className = 'skill__card skill__close';
+    }
+
+    if (itemClass === 'skill__card skill__close') {
+        this.parentNode.className = 'skill__card skill__open'
+    }
+}
+
+skillCardData.forEach((el) => {
+    el.addEventListener('click', toggleSkill)
+})
